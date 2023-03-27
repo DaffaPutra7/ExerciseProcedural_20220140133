8ce1db44163f1dc31b7b7339a68e23a094c46a57
DaffaPutra7 <putradika3604@gmail.com>
2023-03-27 09:39:37 +0700

    Double Rearata

--- ExerciseProcedural_20220140133/ExerciseProcedural_20220140133.cpp
+++ ExerciseProcedural_20220140133/ExerciseProcedural_20220140133.cpp
@@ -5,14 +5,3 @@ double rerata(double a, double b) {
 	return (a + b) / 2;
 }
 
-string status(double rata) {
-	if (rata >= 70)
-		return "Lulus";
-	else
-		return "Gagal";
-}
-
-string status2(double rata, double nil) {
-	if (rata >= 70 || nil >= 80)
-		return"Lulus"
-}
